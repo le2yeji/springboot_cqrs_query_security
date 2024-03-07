cee0c35d0aa4ecd533e45c0e36b1c250d4f85764
le2yeji <leyagi17@gmail.com>
2024-03-07 22:42:54 +0900

    jwt delete

--- src/main/java/com/kt/edu/thirdproject/employee/query/service/EmployeeService.java
+++ src/main/java/com/kt/edu/thirdproject/employee/query/service/EmployeeService.java
@@ -20,21 +20,44 @@ import java.util.List;
 public class EmployeeService {
 
     @Autowired
-    private EmployeeRepository employeeRepository;
+    private EmployeeRepository employeeRepository;    // jasypt로 저장된 비밀번호가  복호화 된다.
+    @Value("${spring.datasource.password}")
+    private String h2Password;
 
-    @Cacheable("employees")
+    /*@Autowired
+    public EmployeeService(EmployeeRepository employeeRepository){
+        this.employeeRepository = employeeRepository;
+
+    }*/
     public List<EmployeeEntity> getEmployeeList() {
         log.info("Request to get all Employees");
+        log.info("h2 password : " + h2Password );
         List<EmployeeEntity> employeeList = new ArrayList<>();
         employeeRepository.findAll().forEach(employeeList::add);
         return employeeList;
     }
 
     // 플러시를 생략해서 dirty checking등을 하지 않으므로 약간의 성능 향상
-    @Cacheable("employee")
     @Transactional(readOnly = true)
     public EmployeeEntity getEmployee(Long id) {
         log.info("Request to get Employee : {}", id);
         return employeeRepository.findById(id).get();
     }
+
+//
+//    @Cacheable("employees")
+//    public List<EmployeeEntity> getEmployeeList() {
+//        log.info("Request to get all Employees");
+//        List<EmployeeEntity> employeeList = new ArrayList<>();
+//        employeeRepository.findAll().forEach(employeeList::add);
+//        return employeeList;
+//    }
+//
+//    // 플러시를 생략해서 dirty checking등을 하지 않으므로 약간의 성능 향상
+//    @Cacheable("employee")
+//    @Transactional(readOnly = true)
+//    public EmployeeEntity getEmployee(Long id) {
+//        log.info("Request to get Employee : {}", id);
+//        return employeeRepository.findById(id).get();
+//    }
 }
--- src/test/java/com/kt/edu/thirdproject/common/JwtAuthenticationControllerTest.java
+++ src/test/java/com/kt/edu/thirdproject/common/JwtAuthenticationControllerTest.java
@@ -1,66 +1,66 @@
-package com.kt.edu.thirdproject.common;
-
-import com.kt.edu.thirdproject.common.controller.JwtAuthenticationController;
-import com.kt.edu.thirdproject.employee.query.service.EmployeeService;
-import org.junit.jupiter.api.Test;
-import com.kt.edu.thirdproject.common.util.JwtTokenUtil;
-import org.junit.jupiter.api.extension.ExtendWith;
-//import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-//import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.web.bind.annotation.CrossOrigin;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-//@RunWith(SpringRunner.class)
-@ExtendWith(SpringExtension.class)
-@WebMvcTest(JwtAuthenticationController.class)
-//@AutoConfigureMockMvc
-@CrossOrigin(origins ="*")
-public class JwtAuthenticationControllerTest {
-
-    @Autowired
-    private MockMvc mockMvc;
-
-    @MockBean
-    private AuthenticationManager authenticationManager;
-
-    @Autowired
-    private JwtTokenUtil jwtTokenUtil;
-
-    @MockBean
-    private EmployeeService employeeQueryService;
-
-    //@Autowired
-    //private ObjectMapper objectMapper;
-
-    @Test
-    public void testCreateAuthenticationToken() throws Exception {
-
-        String testStr = "{\n" +
-                "  \"username\": \"edu\", \n" +
-                "  \"password\": \"edu1234\"\n" +
-                "}";
-
-        //Map<String, String> input = new HashMap<>();
-        // body에 json 형식으로 회원의 데이터를 넣기 위해서 Map을 이용한다.
-        //input.put("username", "edu");
-        //input.put("password", "edu1234");
-
-        mockMvc.perform(post("/api/login")
-                 .content(testStr)
-                //.content(objectMapper.writeValueAsString(input))
-                .contentType(MediaType.APPLICATION_JSON)
-                .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andDo(print());
-    }
-}
+//package com.kt.edu.thirdproject.common;
+//
+//import com.kt.edu.thirdproject.common.controller.JwtAuthenticationController;
+//import com.kt.edu.thirdproject.employee.query.service.EmployeeService;
+//import org.junit.jupiter.api.Test;
+//import com.kt.edu.thirdproject.common.util.JwtTokenUtil;
+//import org.junit.jupiter.api.extension.ExtendWith;
+////import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+//import org.springframework.boot.test.mock.mockito.MockBean;
+//import org.springframework.http.MediaType;
+//import org.springframework.security.authentication.AuthenticationManager;
+//import org.springframework.test.context.junit.jupiter.SpringExtension;
+////import org.springframework.test.context.junit4.SpringRunner;
+//import org.springframework.test.web.servlet.MockMvc;
+//import org.springframework.web.bind.annotation.CrossOrigin;
+//
+//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+//import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+//
+////@RunWith(SpringRunner.class)
+//@ExtendWith(SpringExtension.class)
+//@WebMvcTest(JwtAuthenticationController.class)
+////@AutoConfigureMockMvc
+//@CrossOrigin(origins ="*")
+//public class JwtAuthenticationControllerTest {
+//
+//    @Autowired
+//    private MockMvc mockMvc;
+//
+//    @MockBean
+//    private AuthenticationManager authenticationManager;
+//
+//    @Autowired
+//    private JwtTokenUtil jwtTokenUtil;
+//
+//    @MockBean
+//    private EmployeeService employeeQueryService;
+//
+//    //@Autowired
+//    //private ObjectMapper objectMapper;
+//
+//    @Test
+//    public void testCreateAuthenticationToken() throws Exception {
+//
+//        String testStr = "{\n" +
+//                "  \"username\": \"edu\", \n" +
+//                "  \"password\": \"edu1234\"\n" +
+//                "}";
+//
+//        //Map<String, String> input = new HashMap<>();
+//        // body에 json 형식으로 회원의 데이터를 넣기 위해서 Map을 이용한다.
+//        //input.put("username", "edu");
+//        //input.put("password", "edu1234");
+//
+//        mockMvc.perform(post("/api/login")
+//                 .content(testStr)
+//                //.content(objectMapper.writeValueAsString(input))
+//                .contentType(MediaType.APPLICATION_JSON)
+//                .accept(MediaType.APPLICATION_JSON))
+//                .andExpect(status().isOk())
+//                .andDo(print());
+//    }
+//}
